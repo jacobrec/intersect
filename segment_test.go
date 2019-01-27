dbdd4b06dfcd87802a242f8ef407fe414dfdecb2
Jacob Reckhard <jacobreckhard@gmail.com>
2019-01-27 16:06:31 -0700

    wrote unit tests

--- segment_test.go
+++ segment_test.go
@@ -35,3 +35,108 @@ func TestSegIsPointOn(t *testing.T) {
 		}
 	})
 }
+
+func TestSegIntersectSeg(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewSegment(NewVector(1, 0), NewVector(1, 2))
+		p, b := s1.Intersect(s2)
+		if !b && p.Equals(NewVector(1, 1)) {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("2", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewSegment(NewVector(3, 0), NewVector(3, 2))
+		p, b := s1.Intersect(s2)
+		if b {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("3", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewSegment(NewVector(-1, 0), NewVector(-1, 2))
+		p, b := s1.Intersect(s2)
+		if b {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("4", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewSegment(NewVector(1, 0), NewVector(3, 2))
+		p, b := s1.Intersect(s2)
+		if !b {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+}
+
+func TestSegIntersectRay(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewRay(NewVector(1, 0), NewVector(1, 2))
+		p, b := s1.IntersectRay(s2)
+		if !b && p.Equals(NewVector(1, 1)) {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("2", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewRay(NewVector(3, 0), NewVector(3, 2))
+		p, b := s1.IntersectRay(s2)
+		if !b && p.Equals(NewVector(3, 3)) {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("3", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewRay(NewVector(-1, 0), NewVector(-1, 2))
+		p, b := s1.IntersectRay(s2)
+		if b {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("4", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewRay(NewVector(1, 0), NewVector(3, 2))
+		p, b := s1.IntersectRay(s2)
+		if b {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+}
+
+func TestSegIntersectLine(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewLine(NewVector(1, 0), NewVector(1, 2))
+		p, b := s1.IntersectLine(s2)
+		if !b && p.Equals(NewVector(1, 1)) {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("2", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(3, 3))
+		s2 := NewLine(NewVector(3, 0), NewVector(3, 2))
+		p, b := s1.IntersectLine(s2)
+		if !b && p.Equals(NewVector(3, 3)) {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("3", func(t *testing.T) {
+		s1 := NewSegment(NewVector(-1, -1), NewVector(2, 2))
+		s2 := NewLine(NewVector(-1, 0), NewVector(-1, 2))
+		p, b := s1.IntersectLine(s2)
+		if !b && p.Equals(NewVector(-1, -1)) {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+	t.Run("4", func(t *testing.T) {
+		s1 := NewSegment(NewVector(0, 0), NewVector(2, 2))
+		s2 := NewLine(NewVector(1, 0), NewVector(3, 2))
+		p, b := s1.IntersectLine(s2)
+		if b {
+			t.Error("Failed Test", s1, s2, p, b)
+		}
+	})
+}
